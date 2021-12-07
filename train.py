0dc0fba241fba6693ff6e35149088aa1f8ea3b8f
shangjie-li <1814481684@qq.com>
2021-12-07 18:39:09 +0800

    Commit first version

--- train.py
+++ train.py
@@ -223,7 +223,8 @@ def train():
             args.start_iter = SavePath.from_str(args.resume).iteration
     else:
         print('Initializing weights...')
-        yolact_net.init_weights(backbone_path=args.save_folder + cfg.backbone.path)
+        if cfg.backbone.path is not None:
+            yolact_net.init_weights(backbone_path=args.save_folder + cfg.backbone.path)
 
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,
                           weight_decay=args.decay)
